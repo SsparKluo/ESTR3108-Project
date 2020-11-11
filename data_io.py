315e0d563ee1ef9381310b71e0b67a91b9b0c6c2
SsparKluo <ssparkluo@gmail.com>
2020-11-11 21:59:27 +0800

    add data io

--- data_io.py
+++ data_io.py
@@ -1,78 +1,46 @@
+from os import read
 import numpy as np
 import os
-import gzip
+import sys
 
-def read_seq(seq_file):
+def read_seq_graphprot(seq_file, label = 1):
     seq_list = []
+    labels = []
     seq = ''
-    with gzip.open(seq_file, 'r') as fp:
+    with open(seq_file, 'r') as fp:
         for line in fp:
             if line[0] == '>':
                 name = line[1:-1]
-                if len(seq):
-                    seq_array = get_RNA_seq_concolutional_array(seq)
-                    seq_list.append(seq_array)                    
-                seq = ''
             else:
-                seq = seq + line[:-1]
-        if len(seq):
-            seq_array = get_RNA_seq_concolutional_array(seq)
-            seq_list.append(seq_array) 
+                seq = line[:-1].upper()
+                seq = seq.replace('T', 'U')
+                seq_list.append(seq)
+                labels.append(label)
     
-    return np.array(seq_list)
+    return seq_list, labels
 
-def get_RNA_seq_concolutional_array(seq, motif_len = 10):
-    seq = seq.replace('U', 'T')
-    alpha = 'ACGT'
-
-    #motif: convolutional kernel
-    half_len = motif_len/2
-    row = (len(seq) + half_len *2 )
-    new_array = np.zeros((row, 4))
-    for i in range(half_len):
-        new_array[i] = np.array([0.25]*4)
-    
-    for i in range(row-half_len, row):
-        new_array[i] = np.array([0.25]*4)
-        
-    for i, val in enumerate(seq):
-        i = i + motif_len-1
-        if val not in 'ACGT':
-            new_array[i] = np.array([0.25]*4)
-            continue
-
-        index = alpha.index(val)
-        new_array[i][index] = 1
-
-    return new_array
-
-def load_data_file(inputfile, seq = True, onlytest = False):
-    """
-        Load data matrices from the specified folder.
-    """
-    path = os.path.dirname(inputfile)
-    if len(path):
-        path = './'
+def read_data_file(posi_file, nega_file = None):
     data = dict()
-    if seq: 
-        tmp = []
-        tmp.append(read_seq(inputfile))
-        seq_onehot, structure = read_structure(inputfile, path)
-        tmp.append(seq_onehot)
-        data["seq"] = tmp
-        data["structure"] = structure
-    if onlytest:
-        data["Y"] = []
-    else:
-        data["Y"] = load_label_seq(inputfile)
+    seqs, labels = read_seq_graphprot(posi_file, label = 1)
+    if nega_file:
+        seqs2, labels2 = read_seq_graphprot(nega_file, label = 0)
+        seqs = seqs + seqs2
+        labels = labels + labels2
         
+    data["seq"] = seqs
+    data["Y"] = np.array(labels)
+    
     return data
 
-
+def main():
+    posi = input('positive test file path: ')
+    nega = input('negative test file path: ')
+    data = read_data_file(posi, nega_file= nega)
+    print(data["seq"])
+    print(data["Y"])
 
 
 
 if __name__ == "__main__":
-    dataset = sys.argv[1]
-    run_ideepv(dataset)
+    main()
     
