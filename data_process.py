bd85dec9d49e281f2b9b8c862f23353dab93f271
SsparKluo <ssparkluo@gmail.com>
2020-11-12 10:36:32 +0800

    add a mthod for transforming seq

--- data_process.py
+++ data_process.py
@@ -36,6 +36,34 @@ def split_seq(seq, overlap = 20, window_size = 101):
     splitted_seqs.append(remain_seq)
     return splitted_seqs
 
+def seq2matrix(seq, motif_len = 4):
+    seq = seq.replace('U', 'T')
+    alpha = 'ACGT'
+    #for seq in seqs:
+    #for key, seq in seqs.iteritems():
+    row = (len(seq) + 2*motif_len - 2)
+    new_array = np.zeros((row, 4))
+    for i in range(motif_len-1):
+        new_array[i] = np.array([0.25]*4)
+    
+    for i in range(row-3, row):
+        new_array[i] = np.array([0.25]*4)
+        
+    #pdb.set_trace()
+    for i, val in enumerate(seq):
+        i = i + motif_len-1
+        if val not in 'ACGT':
+            new_array[i] = np.array([0.25]*4)
+            continue
+        #if val == 'N' or i < motif_len or i > len(seq) - motif_len:
+        #    new_array[i] = np.array([0.25]*4)
+        #else:
+        index = alpha.index(val)
+        new_array[i][index] = 1
+
+        #data[key] = new_array
+    return new_array
+
 
 
 if __name__ == "__main__":
